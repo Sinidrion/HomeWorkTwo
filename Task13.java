ebb419112be81cc1da1d3a1c7bbaf0cbfa6607cb
Sinidrion <52216001+Sinidrion@users.noreply.github.com>
2019-06-27 21:30:40 +0300

    Add files via upload

---
+++ Task13.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Task13 {
+
+	public static void main(String[] args) {
+		System.out.println("Введите число");
+		Scanner in = new Scanner(System.in);
+		int num = in.nextInt();
+		System.out.println("Вы ввели число: "+ num);
+	}
+}
+		
