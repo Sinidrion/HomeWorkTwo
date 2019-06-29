8be58b0cc3b94f99a6b2cd81c218142063a0a3ee
Sinidrion <52216001+Sinidrion@users.noreply.github.com>
2019-06-30 00:47:44 +0300

    Add files via upload

---
+++ Task160.java
@@ -0,0 +1,15 @@
+public class Task160 {
+
+    public static void main(String[] args) {
+        int a;
+        int b;
+        int c = 1;
+        b = c;
+        a = b;
+        c = a;
+        b = a;
+        c = b;
+        a = c;
+    }
+}
+
