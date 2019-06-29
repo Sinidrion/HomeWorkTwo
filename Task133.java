dca3fc812866580337a439b015ab7e87b22f95bc
Sinidrion <52216001+Sinidrion@users.noreply.github.com>
2019-06-29 23:54:47 +0300

    Add files via upload

---
+++ Task133.java
@@ -0,0 +1,12 @@
+public class Task133 {
+
+    public static void main(String[] args) {
+        float numberOfPeople = 9508000f;
+        float landArea = 207595f;
+        float populationDensity = numberOfPeople / landArea;
+        System.out.println("Численность населения Беларуси равна " + populationDensity + " человек на квадратный киломметр");
+
+
+
+    }
+}
