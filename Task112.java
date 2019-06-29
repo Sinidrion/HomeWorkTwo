fe379e13f8f62ca5a2f852f581cac20312d93563
Sinidrion <52216001+Sinidrion@users.noreply.github.com>
2019-06-29 16:18:51 +0300

    Add files via upload

---
+++ Task112.java
@@ -0,0 +1,28 @@
+package com.company;
+import java.util.Scanner;
+
+public class Task112 {
+
+    public static void main(String[] args) {
+        int c=5;
+        int d=10;
+        int e=7;
+        int f=100;
+        int g=1949;
+        int h=25;
+        System.out.println("Задайте значение переменной a");
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        System.out.println("Задайте значение переменной b");
+        Scanner inn = new Scanner(System.in);
+        int b = inn.nextInt();
+        System.out.println("Задайте значение переменной x");
+        Scanner innn = new Scanner(System.in);
+        int x = innn.nextInt();
+        System.out.println("Задайте значение переменной y");
+        Scanner innnn = new Scanner(System.in);
+        int y = innnn.nextInt();
+        System.out.println("а) 2 кг    б) " + a + "1    в) " + x +" " + y);
+        System.out.println("   13 17      19 " + b + "     5 " + y);
+    }
+}
